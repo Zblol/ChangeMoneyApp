6222f66231b70b89c15cdf526a1d21d41b9480b5
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2020-07-29 15:30:19 +0300

    add scanActivity

--- app/src/main/java/com/app/zblol/changermoneyapps/MainActivity.java
+++ app/src/main/java/com/app/zblol/changermoneyapps/MainActivity.java
@@ -1,13 +1,43 @@
 package com.app.zblol.changermoneyapps;
 
+import android.content.Intent;
+import android.support.design.widget.TabItem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabItem scanTab;
+    TabItem currencyTab;
+
+    View.OnClickListener scanChange = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(),ScanActivity.class);
+            startActivity(intent);
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scanTab = findViewById(R.id.scanTab);
+        currencyTab = findViewById(R.id.currencyTab);
+
+
     }
 }
